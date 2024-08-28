5250d6fcd99a30e60fe9ae5da11fd788df3f96df
Muhammad Saqib <muhammad@creativefolks.co.uk>
2024-08-28 15:30:43 +0100

    property pages and markup fix

--- wp-content/themes/mccartneys/propertyhive/content-single-property.php
+++ wp-content/themes/mccartneys/propertyhive/content-single-property.php
@@ -72,6 +72,9 @@ $virtual_tour_urls = $property->get_virtual_tour_urls();
                         class="badge-icon">
                     <a id="galleryMedia">1/<?php echo $galleryAttachmentCount ?></a>
                 </div>
+                <div class="gallery-link-cr mcc-badge">
+                    <a id="galleryMedia">1/<?php echo $galleryAttachmentCount ?></a>
+                </div>
             </div>
 
             <div class="side-images">
@@ -79,6 +82,9 @@ $virtual_tour_urls = $property->get_virtual_tour_urls();
                     class="property-secondary-image" alt="><?php the_title(); ?>">
                 <img src="<?php echo wp_get_attachment_url($gallery_attachments[2], $size = 'property-square') ?>"
                     class="property-secondary-image" alt="><?php the_title(); ?>">
+                    <div class="gallery-link-cr mcc-badge">
+                    <a id="galleryMedia">1/<?php echo $galleryAttachmentCount ?></a>
+                </div>
             </div>
         </div>
 
