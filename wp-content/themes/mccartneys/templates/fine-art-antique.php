773c35c6e6741279db66f55c26d0a61deeba19a4
Muhammad Saqib <muhammad@creativefolks.co.uk>
2024-09-04 21:32:06 +0100

    Fix fine and art page

--- wp-content/themes/mccartneys/templates/fine-art-antique.php
+++ wp-content/themes/mccartneys/templates/fine-art-antique.php
@@ -174,7 +174,8 @@ if ( !empty( $image_private ) ) { ?>
                         <img src="<?php echo $ac_image['url']; ?>" alt="<?php echo $ac_image['alt']; ?>" class="w-100">
                     <?php endif; ?>
                     <ul class="info-box">
-                    <li>
+                         <?php if( get_sub_field('livestock_auctioneers_number') ): ?>
+                              <li>
                             <i class="fa-solid fa-phone"></i>
                             <a href="tel:<?php the_sub_field('livestock_auctioneers_number'); ?>"><?php the_sub_field('livestock_auctioneers_number'); ?></a>
                             </li>
