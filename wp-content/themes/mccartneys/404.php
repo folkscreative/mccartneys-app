6c524ee045906c279f3abfb1bdac138c323011da
Muhammad Saqib <muhammad@creativefolks.co.uk>
2024-09-13 18:29:46 +0100

    Fix 404 page layout CSS

--- wp-content/themes/mccartneys/404.php
+++ wp-content/themes/mccartneys/404.php
@@ -12,8 +12,7 @@ get_header();
 		<section class="general-page-outer error-404">
            <div class="container">
                <div class="content">
-			   <h1 class="page-title"><span>ERROR 404 <span> <br> We couldn't find the page you are looking for!
-				</h1>
+			   <h1 class="page-title"> <span>ERROR 404 <span> We couldn't find the page you are looking for! </h1>
 				<a href="#https://wordpress-1285863-4695980.cloudwaysapps.com" class="btn-cs-dark">Go Back</a>
                </div>
         </div>
