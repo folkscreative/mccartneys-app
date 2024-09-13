020d36431812ffca92351666bab3d4ae196a494b
Muhammad Saqib <muhammad@creativefolks.co.uk>
2024-09-13 18:11:59 +0100

    Fix 404 page

--- wp-content/themes/mccartneys/404.php
+++ wp-content/themes/mccartneys/404.php
@@ -9,52 +9,20 @@
 
 get_header();
 ?>
+		<section class="general-page-outer">
+           <div class="container">
+               <div class="content">
+			   <h1 class="page-title">
+					ERROR 404 <br>
+				We couldn't find the page you are looking for!
+				</h1>
+				<a href="#https://wordpress-1285863-4695980.cloudwaysapps.com" class="btn-cs-dark">Go Back</a>
+               </div>
+        </div>
+        </section>
+  
 
-	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mccartneys' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mccartneys' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'mccartneys' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$mccartneys_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'mccartneys' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$mccartneys_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
 
 <?php
 get_footer();
