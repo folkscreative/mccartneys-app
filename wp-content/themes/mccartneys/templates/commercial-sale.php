12f8f7cbd77cccbc1078a9d7f458cc4bd59d6f7e
Sajid Ahmed <sajidahmed0300@gmail.com>
2024-11-27 17:34:26 +0000

    Fix search filter

--- wp-content/themes/mccartneys/templates/commercial-sale.php
+++ wp-content/themes/mccartneys/templates/commercial-sale.php
@@ -111,16 +111,11 @@ if ( !empty( $image_private ) ) { ?>
                 </div>
                 <div class="tab-pane fade" id="rent" role="tabpanel" aria-labelledby="tab-rent">
                     <div class="inner-tabs pr">
-                        <?php echo do_shortcode('[recent_properties department="commercial"]');?>
+                        <?php echo do_shortcode('[recent_properties _parent_department="Lettings"]');?>
                     </div>
-            <a class="btn-cs-dark"
-                href="<?php 
-                    $archive_url = get_post_type_archive_link('property');
-                    $full_url = add_query_arg('_parent_department', 'Lettings', $archive_url);
-                    echo apply_filters('propertyhive_search_form_action', $full_url); 
-                ?>">
-                View all properties
-                </a>
+                    <a class="btn-cs-dark"
+                        href="<?php echo apply_filters('propertyhive_search_form_action', get_post_type_archive_link( 'property' ) ); ?>">View
+                        all properties</a>
                 </div>
 
                 <div class="tab-pane fade" id="auction" role="tabpanel" aria-labelledby="tab-auction">
