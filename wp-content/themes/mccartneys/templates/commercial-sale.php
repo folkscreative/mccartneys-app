b1a26fed2362505381b77fa751c2a73bbdc8e84f
Sajid Ahmed <sajidahmed0300@gmail.com>
2024-11-29 11:13:09 +0000

    fix parent depar

--- wp-content/themes/mccartneys/templates/commercial-sale.php
+++ wp-content/themes/mccartneys/templates/commercial-sale.php
@@ -102,7 +102,7 @@ if ( !empty( $image_private ) ) { ?>
 
             <div class="tab-pane fade show active" id="sale" role="tabpanel" aria-labelledby="tab-sale">
                     <div class="inner-tabs pr">
-                        <?php echo do_shortcode('[recent_properties department="commercial"]');?>
+                        <?php echo do_shortcode('[recent_properties _parent_department="Sales"]');?>
                     </div>
                     <a class="btn-cs-dark"
                         href="<?php echo apply_filters('propertyhive_search_form_action', get_post_type_archive_link( 'property' ) ); ?>department=commercial">View
