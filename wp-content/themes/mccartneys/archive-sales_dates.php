0e60cf70c27ee69bcab043343d2a49a982b032f4
Sajid Ahmed <sajidahmed0300@gmail.com>
2024-12-03 19:36:33 +0000

    Fix links issue

--- wp-content/themes/mccartneys/archive-sales_dates.php
+++ wp-content/themes/mccartneys/archive-sales_dates.php
@@ -115,7 +115,7 @@ get_header();
                 while ($filtered_query->have_posts()) : $filtered_query->the_post(); ?>
   
                 <div class="show-dates-content">
-                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <h3><?php the_title(); ?></h3>
                     <?php echo get_post_meta(get_the_ID(), 'show_date', true); ?>
                     <?php echo get_post_meta(get_the_ID(), 'location', true); ?>
                     <?php
@@ -167,55 +167,55 @@ get_header();
     </section>
     <!-- Show Dates ends -->
     
-     <!-- Departments -->
-     <section class="departments other">
-        <div class="container">
-            <div class="content">
-                <?php if( get_field('our_departments_title', 'option') ): ?>
-                        <h2><?php the_field('our_departments_title', 'option'); ?></h2>
-                    <?php endif; ?>
-                    <?php if( get_field('our_departments_description', 'option') ): ?>
-                        <p><?php the_field('our_departments_description', 'option'); ?></p>
-                    <?php endif; ?>
-            </div>
-            <?php if( have_rows('our_departments_slider', 'option') ): ?>
-            <div class="depart-slider depar">
-            <?php while( have_rows('our_departments_slider', 'option') ): the_row(); ?>
-            <div class="slide-wrap">
-            <?php 
-                        $department_cartneys_slider_link = get_sub_field('department_slider_button', 'option');
-                        if( $department_cartneys_slider_link ): 
-                            $department_cartneys_slider_link_url = $department_cartneys_slider_link['url'];
-                            $department_cartneys_slider_link_title = $department_cartneys_slider_link['title'];
-                            $department_cartneys_slider_link_target = $department_cartneys_slider_link['target'] ? $department_cartneys_slider_link['target'] : '_self';
-                            ?>
-                            <a class="btn-transparent" href="<?php echo esc_url( $department_cartneys_slider_link_url ); ?>" target="<?php echo esc_attr( $department_cartneys_slider_link_target ); ?>"><?php echo esc_html( $department_cartneys_slider_link_title ); ?><span><i class="fa-solid fa-angle-right"></i></span></a>
-                        <?php endif; ?>
-            <?php
-                $department_slider_bg_image = get_sub_field('our_departments_thumbnail', 'option');
-                if( !empty($department_slider_bg_image) ):?>
-                <img src="<?php echo $department_slider_bg_image['url']; ?>" alt="<?php echo $department_slider_bg_image['alt']; ?>">
+<!-- Departments -->
+<section class="departments other">
+<div class="container">
+    <div class="content">
+        <?php if( get_field('our_departments_title', 'option') ): ?>
+                <h2><?php the_field('our_departments_title', 'option'); ?></h2>
+            <?php endif; ?>
+            <?php if( get_field('our_departments_description', 'option') ): ?>
+                <p><?php the_field('our_departments_description', 'option'); ?></p>
+            <?php endif; ?>
+    </div>
+    <?php if( have_rows('our_departments_slider', 'option') ): ?>
+    <div class="depart-slider depar">
+    <?php while( have_rows('our_departments_slider', 'option') ): the_row(); ?>
+    <div class="slide-wrap">
+    <?php 
+                $department_cartneys_slider_link = get_sub_field('department_slider_button', 'option');
+                if( $department_cartneys_slider_link ): 
+                    $department_cartneys_slider_link_url = $department_cartneys_slider_link['url'];
+                    $department_cartneys_slider_link_title = $department_cartneys_slider_link['title'];
+                    $department_cartneys_slider_link_target = $department_cartneys_slider_link['target'] ? $department_cartneys_slider_link['target'] : '_self';
+                    ?>
+                    <a class="btn-transparent" href="<?php echo esc_url( $department_cartneys_slider_link_url ); ?>" target="<?php echo esc_attr( $department_cartneys_slider_link_target ); ?>"><?php echo esc_html( $department_cartneys_slider_link_title ); ?><span><i class="fa-solid fa-angle-right"></i></span></a>
                 <?php endif; ?>
-                <div class="inner-content">
-                    <h3><?php the_sub_field('department_cart_title', 'option'); ?></h3>
-                    <p><?php the_sub_field('department_cart_description', 'option'); ?></p>
-                    <?php 
-                        $department_cartneys_slider_link = get_sub_field('department_slider_button', 'option');
-                        if( $department_cartneys_slider_link ): 
-                            $department_cartneys_slider_link_url = $department_cartneys_slider_link['url'];
-                            $department_cartneys_slider_link_title = $department_cartneys_slider_link['title'];
-                            $department_cartneys_slider_link_target = $department_cartneys_slider_link['target'] ? $department_cartneys_slider_link['target'] : '_self';
-                            ?>
-                            <a class="btn-cs-light" href="<?php echo esc_url( $department_cartneys_slider_link_url ); ?>" target="<?php echo esc_attr( $department_cartneys_slider_link_target ); ?>"><?php echo esc_html( $department_cartneys_slider_link_title ); ?><span><i class="fa-solid fa-angle-right"></i></span></a>
-                        <?php endif; ?>
-                </div>
-                </div>
-                <?php endwhile; ?>
-                </div>
-            <?php endif; ?>                     
+    <?php
+        $department_slider_bg_image = get_sub_field('our_departments_thumbnail', 'option');
+        if( !empty($department_slider_bg_image) ):?>
+        <img src="<?php echo $department_slider_bg_image['url']; ?>" alt="<?php echo $department_slider_bg_image['alt']; ?>">
+        <?php endif; ?>
+        <div class="inner-content">
+            <h3><?php the_sub_field('department_cart_title', 'option'); ?></h3>
+            <p><?php the_sub_field('department_cart_description', 'option'); ?></p>
+            <?php 
+                $department_cartneys_slider_link = get_sub_field('department_slider_button', 'option');
+                if( $department_cartneys_slider_link ): 
+                    $department_cartneys_slider_link_url = $department_cartneys_slider_link['url'];
+                    $department_cartneys_slider_link_title = $department_cartneys_slider_link['title'];
+                    $department_cartneys_slider_link_target = $department_cartneys_slider_link['target'] ? $department_cartneys_slider_link['target'] : '_self';
+                    ?>
+                    <a class="btn-cs-light" href="<?php echo esc_url( $department_cartneys_slider_link_url ); ?>" target="<?php echo esc_attr( $department_cartneys_slider_link_target ); ?>"><?php echo esc_html( $department_cartneys_slider_link_title ); ?><span><i class="fa-solid fa-angle-right"></i></span></a>
+                <?php endif; ?>
         </div>
-     </section>
-     <!-- Departments ends -->
+        </div>
+        <?php endwhile; ?>
+        </div>
+    <?php endif; ?>                     
+</div>
+</section>
+<!-- Departments ends -->
 
 
 <?php get_footer();?>
