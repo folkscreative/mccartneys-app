17206cee7fa87ef33712e2b3b084692452b443ce
Sajid Ahmed <sajidahmed0300@gmail.com>
2024-12-04 19:07:02 +0000

    Team popup js and css fixed

--- wp-content/themes/mccartneys/assets/js/custom.js
+++ wp-content/themes/mccartneys/assets/js/custom.js
@@ -184,7 +184,7 @@ jQuery(document).ready(function ($) {
         dots: true,
         arrows: true,
         infinite: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         slidesToShow: 4,
         slideToScroll: 1,
