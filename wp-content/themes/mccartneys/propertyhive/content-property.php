b9d214cde40d07e1b4922d7281bf18e893ec4efc
Sajid Ahmed <sajidahmed0300@gmail.com>
2024-12-23 17:53:47 +0000

    revert carousal

--- wp-content/themes/mccartneys/propertyhive/content-property.php
+++ wp-content/themes/mccartneys/propertyhive/content-property.php
@@ -12,8 +12,6 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $property, $propertyhive_loop;
-$gallery_attachments = $property->get_gallery_attachment_ids();
-$galleryAttachmentCount = count($gallery_attachments);
 
 // Store loop count we're currently on
 if ( empty( $propertyhive_loop['loop'] ) )
@@ -42,54 +40,65 @@ if ( $property->featured == 'yes' )
 <div <?php post_class( $classes ); ?>>
 
     <div class="col-left">
-        <div class="swiper-container property-carousel">
-            <div class="swiper-wrapper">
-                <!-- Main Image -->
-                <div class="swiper-slide">
-                    <a href="<?php the_permalink(); ?>">
-                        <img src="<?php echo $property->get_main_photo_src( $size = 'large' ); ?>" class="property-featured-image" alt="<?php the_title(); ?>">
-                    </a>
-                </div>
-                <!-- Gallery Images -->
-                <?php if ( $galleryAttachmentCount > 0 ) : ?>
-                    <?php foreach ( $gallery_attachments as $attachment_id ) : ?>
-                        <div class="swiper-slide">
-                            <a href="<?php the_permalink(); ?>">
-                                <img src="<?php echo wp_get_attachment_image_url( $attachment_id, 'large' ); ?>" class="property-featured-image" alt="<?php the_title(); ?>">
-                            </a>
-                        </div>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-            </div>
-            <!-- Add Pagination and Navigation -->
-            <div class="swiper-pagination"></div>
-            <div class="swiper-button-next"></div>
-            <div class="swiper-button-prev"></div>
-        </div>
+        <a href="<?php the_permalink(); ?>">
+            <img src="<?php echo $property->get_main_photo_src( $size = 'large' ) ?>" class="property-featured-image"
+                alt="><?php the_title(); ?>">
+        </a>
     </div>
-
     <div class="col-right">
         <span class="price-info price-qualifier"><?php echo $property->price_qualifier; ?></span>
         <h3 class="price-info price"><?php echo $property->get_formatted_price(); ?></h3>
 
         <ul class="features">
-            <!-- Feature Items -->
-            <?php if (!is_null($property->property_type) && trim($property->property_type) !== ''): ?>
-                <li>
-                    <img class="feature-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-property-type.svg" alt="">
-                    <span><?php echo $property->property_type; ?></span>
-                </li>
-            <?php endif; ?>
+
+            <?php if (!is_null($property->property_type) && $property->property_type !== '' && trim($property->property_type) !== '') { ?>
+            <!-- <li>
+                <img class="feature-icon"
+                    src="<?php echo get_template_directory_uri(); ?>/assets/images/icon-property-type.svg" alt="">
+                <span class=""><?php echo $property->property_type; ?></span>
+            </li> -->
+            <?php }?>
             <?php if ( $property->bedrooms > 0 ): ?>
-                <li>
-                    <img class="feature-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/bed-vector.svg" alt="">
-                    <span><?php echo $property->bedrooms; ?></span>
-                </li>
+            <li>
+                <img class="feature-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/bed-vector.svg"
+                    alt="">
+                <span><?php echo $property->bedrooms; ?></span>
+            </li>
             <?php endif; ?>
-            <!-- Add other feature conditions here -->
+
+            <?php if ( $property->bathrooms > 0 ): ?>
+            <li>
+                <img class="feature-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/bath-logo.svg"
+                    alt="">
+                <span><?php echo $property->bathrooms; ?></span>
+            </li>
+            <?php endif; ?>
+            <?php if ( $property->reception_rooms > 0 ): ?>
+            <li>
+                <img class="feature-icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/recep-room.png"
+                    alt="">
+                <span><?php echo $property->reception_rooms; ?></span>
+            </li>
+            <?php endif; ?>
+
+            <?php if (
+    				(!is_null($property->floor_area_to_sqft) && $property->floor_area_to_sqft !== '' && $property->floor_area_to_sqft != 0) ||
+    				(!is_null($property->floor_area_from_sqft) && $property->floor_area_from_sqft !== '' && $property->floor_area_from_sqft != 0)
+				)  { ?>
+
+
+            <li>
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/sq-ft-logo.svg" alt="">
+                <span><?php echo $property->get_formatted_floor_area(); ?></span>
+            </li>
+            <?php }?>
+
+
+
         </ul>
         <h4 class="property-address"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
         <p><?php echo mb_strimwidth(get_the_excerpt(), 0, 250, "..."); ?></p>
+
     </div>
 
     <?php do_action( 'propertyhive_after_search_results_loop_item' ); ?>
